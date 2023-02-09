90f7099d0d7e667e0afdd6cb467699272de31a2b
gywns1720 <gywns1720@gmail.com>
2023-02-09 14:04:55 +0900

    스프링 부트 프로젝트 시작하기

---
+++ src/main/java/com/fastcampus/FastCampusProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastCampusProjectBoardApplication {
+    // 스프링부트 어플리케이션 시작지점
+    public static void main(String[] args) {
+        SpringApplication.run(FastCampusProjectBoardApplication.class, args);
+    }
+
+}
